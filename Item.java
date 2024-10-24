d0c79bddbde91e2257cb5b8de0caa6c127b209a5
GabeB <71792679+gBenninghoff@users.noreply.github.com>
2024-10-24 02:42:33 -0400

    Add files via upload
    First commit, just started

---
+++ Item.java
@@ -0,0 +1,21 @@
+public class Item {
+    private int weight; //each item is required to have some weight
+    private String item; //each item needs to be called something
+
+    public int getWeight() {
+        return weight;
+    }
+
+    public void setWeight(int weight) {
+        if(weight )
+        this.weight = weight;
+    }
+
+    public String getItem() {
+        return item;
+    }
+
+    public void setItem(String item) {
+        this.item = item;
+    }
+}
